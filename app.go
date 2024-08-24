bea091118f1d43380d8ad770742a5ac72a014f6c
Sarthak Srivastava <2016ceb1017@iitrpr.ac.in>
2024-08-24 23:45:29 +0530

    Appending the last function to all

--- app.go
+++ app.go
@@ -1,23 +1,28 @@
 package main
 
 import (
+	"example/my-app/arrayPractice"
+	"example/my-app/bank"
 	"example/my-app/fileOps"
+	"example/my-app/funcPractice"
+	investmentcalculator "example/my-app/investment_calculator"
+	"example/my-app/mapPractice"
+	"example/my-app/notes"
 	"example/my-app/price"
+	profitcalculator "example/my-app/profitCalculator"
+	structure "example/my-app/struct"
 	"fmt"
 )
 
-// func main() {
-// 	investmentcalculator.CalculateInvestmentReturn()
-// 	profitcalculator.CalculateProfit()
-// 	bank.BankOperations()
-// 	structure.StructOperation()
-// 	notes.NotesAppOperation()
-// 	arrayPractice.ArrayPractice()
-// 	mapPractice.MapPractice()
-// 	funcPractice.FuncPractice()
-// }
-
 func main() {
+	investmentcalculator.CalculateInvestmentReturn()
+	profitcalculator.CalculateProfit()
+	bank.BankOperations()
+	structure.StructOperation()
+	notes.NotesAppOperation()
+	arrayPractice.ArrayPractice()
+	mapPractice.MapPractice()
+	funcPractice.FuncPractice()
 	taxes := []float64{10, 30, 50}
 
 	for _, tax := range taxes {
